dfb47eb04a9e103774039a286753aba1f933df05
shadab <gabuyama68@gmail.com>
2024-03-28 20:42:47 +0530

    wip

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Sk Alauddin",
-  profile: "/github-pages/profile.png",
+  profile: "/profile.png",
   designation: "App/Web Developer & Digital Marketer",
   description:
     "I'm Web Developer/ App Developer and Digital Marketer from Kolkata India. I enjoy turning complex problems into simple, beautiful and intuitive designs.My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way. I created web design for many famous brand companies.",
