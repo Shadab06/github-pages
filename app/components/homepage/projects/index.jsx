09bb8818f4084b2bf8c1ac8c30fb23eb05075de0
shadab <gabuyama68@gmail.com>
2024-03-30 00:37:24 +0530

    hat

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,12 +1,11 @@
-import { projectsData } from '@/utils/data/projects-data';
-import ProjectCard from './project-card';
-import Link from 'next/link';
-import { FaArrowRight } from 'react-icons/fa';
+import { projectsData } from "@/utils/data/projects-data";
+import ProjectCard from "./project-card";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 
 const Projects = () => {
-
   return (
-    <div id='projects' className="relative z-50  my-12 lg:my-24">
+    <div id="projects" className="relative z-50  my-12 lg:my-24">
       <div className="sticky top-10">
         <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
         <div className="flex items-center justify-start relative">
@@ -36,17 +35,15 @@ const Projects = () => {
         <Link
           className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
           role="button"
-          href="/project/index.html"
-          target='_blank'
+          href="https://digitalgenieji.github.io/project/"
+          target="_blank"
         >
           <span>View More</span>
           <FaArrowRight size={16} />
         </Link>
       </div>
     </div>
-   
   );
-  
 };
 
 export default Projects;
